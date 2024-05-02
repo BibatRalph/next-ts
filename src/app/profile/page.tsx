d688e43552b130bccb0a52177ec0b9dd2eda9b13
r.bibat <r.bibat@DESKTOP-9SFJGFS>
2024-05-02 14:11:36 +0800

    Commit: Basic routing and API fetch

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Metadata } from 'next';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+    title: 'Profile',
+  }
+
+  export default function Page() {
+    return (
+      <div>
+        <h1>Profile Page</h1>
+        <Link href="/">Home</Link>
+      </div>
+    );
+  }
+
+  
