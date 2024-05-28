6c297e0332ee9530edc2729d871f636ea22a1e53
r.bibat <r.bibat@DESKTOP-9SFJGFS>
2024-05-28 14:20:56 +0800

    Commit: CSS only horizontal scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,24 +27,30 @@ export default async function Page() {
  
   return (
     <>
-      <div className="flex flex-col h-screen">
+      {/* <div className="flex flex-col h-screen">
       <div className="flex-1 flex">
         <div className="w-1/2 bg-blue-100 p-4 rounded-l-lg relative">
-          {/* Left box content */}
           <p className="absolute bottom-4 text-xl text-black-800">Large Text</p>
           <p className="absolute bottom-10 text-xl text-black-800">Large Text</p>
         </div>
-        <div className="w-1/2 bg-green-100 p-4 rounded-r-lg relative">
-          {/* Right box content */}
-          <div className="h-48 w-48 absolute bottom-4 bg-blue-100 p-4 flex justify-center">
-      </div>
+        <div className="w-1/2 bg-green-100 rounded-r-lg relative">
+          <div className="w-full absolute bg-red-100 p-6 flex justify-center">
+          </div>
         </div>
       </div>
       <div className="h-1/2 bg-red-100 p-4 flex justify-center">
-        {/* Bottom box content */}
         <p className="bottom-4 text-sm text-black-800">small text</p>
       </div>
+    </div> */}
+
+    <div id="container">
+    <div id="container2">
+      <div className="box one"><div>1</div></div>
+      <div className="box two"><div>2</div></div>
+      <div className="box three"><div>3</div></div>
+      <div className="box four"><div>Last</div></div>
     </div>
+  </div>
     </>                                                                                                             
   )
 }
